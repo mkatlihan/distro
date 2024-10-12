68ed9ff955faea818172b6a220c14c2122c3a0a0
Mustafa Atlihan <mkatlihan@gmail.com>
2024-10-12 12:19:21 +0300

    setpaths

---
+++ setpaths.bat
@@ -0,0 +1,37 @@
+set NEW_LUA_CPATH=%TORCH_INSTALL_DIR%\luarocks\systree\lib\lua\5.1\?.dll;;
+set NEW_LUA_PATH=%TORCH_INSTALL_DIR%\luarocks\lua\?.lua;%TORCH_INSTALL_DIR%\luarocks\lua\?\init.lua;%TORCH_INSTALL_DIR%\luarocks\systree\share\lua\5.1\?.lua;%TORCH_INSTALL_DIR%\luarocks\systree\share\lua\5.1\?\init.lua;;
+set NEW_PATH=%TORCH_INSTALL_DIR%;%TORCH_INSTALL_DIR%\bin;%TORCH_INSTALL_DIR%\luarocks;%TORCH_INSTALL_DIR%\luarocks\tools;%TORCH_INSTALL_DIR%\luarocks\systree\bin;;%TORCH_INSTALL_DIR%\bin\graphviz;%PATH%;;
+set TORCHACTIVATE_CMD=h:\dev\Torch\distro.git\.\install\torch-activate.cmd
+set TORCH_DISTRO=h:\dev\Torch\distro.git\.
+set TORCH_INSTALL_BIN=h:\dev\Torch\distro.git\.\install\bin
+set TORCH_INSTALL_DIR=h:\dev\Torch\distro.git\.\install
+set TORCH_INSTALL_INC=h:\dev\Torch\distro.git\.\install\include
+set TORCH_INSTALL_LIB=h:\dev\Torch\distro.git\.\install\lib
+set TORCH_INSTALL_ROC=h:\dev\Torch\distro.git\.\install\luarocks
+set TORCH_LUAJIT_BRANCH=v2.1
+set TORCH_LUAROCKS_LUA=5.1
+set TORCH_LUA_SOURCE=luajit-2.1
+set TORCH_LUA_VERSION=LUAJIT21
+set TORCH_SETUP_FAIL=0
+set TORCH_SETUP_HAS_CUDA=1
+set TORCH_VS_PLATFORM=x64
+set TORCH_VS_TARGET=x64
+set TORCH_VS_VERSION=17
+set LUAJIT_CMD=h:\dev\Torch\distro.git\.\install\luajit.cmd
+set LUAROCKS_CMD=h:\dev\Torch\distro.git\.\install\luarocks.cmd
+set LUAROCKS_CONFIG=h:\dev\Torch\distro.git\.\install\luarocks\config-5.1.lua
+set LUA_DEV=e:\usr\Lua
+set NEW_LUA_CPATH=%TORCH_INSTALL_DIR%\luarocks\systree\lib\lua\5.1\?.dll;;
+set NEW_LUA_PATH=%TORCH_INSTALL_DIR%\luarocks\lua\?.lua;%TORCH_INSTALL_DIR%\luarocks\lua\?\init.lua;%TORCH_INSTALL_DIR%\luarocks\systree\share\lua\5.1\?.lua;%TORCH_INSTALL_DIR%\luarocks\systree\share\lua\5.1\?\init.lua;;
+set TORCH_LUAJIT_BRANCH=v2.1
+set TORCH_LUAROCKS_LUA=5.1
+set TORCH_LUA_SOURCE=luajit-2.1
+set TORCH_LUA_VERSION=LUAJIT21
+set "INTEL_MKL_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.2.185\windows\mkl"
+set "INTEL_COMPILER_DIR=C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2018.2.185\windows\compiler"
+
+set "TORCH_SETUP_HAS_MKL=1"
+set WIN_DLFCN_INCDIR=%TORCH_DISTRO:\=\\%\\win-files\\3rd\\dlfcn-win32\\include
+set WIN_DLFCN_LIBDIR=%TORCH_DISTRO:\=\\%\\win-files\\3rd\\dlfcn-win32\\lib
+
+set LUAROCKS_CMD=%TORCH_INSTALL_DIR%\luarocks.cmd
